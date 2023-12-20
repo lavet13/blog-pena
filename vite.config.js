9887e40fe5a0118bed24fa5e5c68aba5c1aa1a33
Ivan <lavet13@mail.ru>
2023-12-20 21:57:33 +0300

    works

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,15 @@
-export default {
-  base: '/',
-};
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        'day-one': resolve(__dirname, 'blogs/day-one/index.html'),
+        // 'day-two': resolve(__dirname, 'blogs/day-two/index.html'),
+        // 'day-three': resolve(__dirname, 'blogs/day-three/index.html'),
+      },
+    },
+  },
+});
